db344e05d5de36870b7ea3bcf6f62dcdde850515
broerjuang <juangwiantoro@gmail.com>
2016-11-06 22:04:24 +0700

    testing using mocha is done

--- test.js
+++ test.js
@@ -1,6 +1,5 @@
 'use strict'
 
-'use strict'
 const assert = require('assert');
 const lib = require('./controller');
 const data = require('./data/data')
@@ -33,61 +32,20 @@ describe('#findByYear',() => {
   });
 });
 
-//
-// // count
-//
-// describe('#C.count',() => {
-//   it('should return 0 when the value inside the array is not present', () => {
-//     assert.equal(0, C.count(empty));
-//   });
-//   it(`should return 4 when array has four values inside it`, () => {
-//     assert.equal(fill.length, C.count(fill));
-//   });
-//   it(`should return 3 when array has 3 objects inside it`, () => {
-//     assert.equal(nested.length, C.count(nested));
-//   });
-// });
-//
-// describe('#C.head',() => {
-//   it('should return [] when the array has no value', () => {
-//     assert.deepEqual([], C.head(empty));
-//   });
-//   it('should return [1] when the array is [1,2,3,4]', () => {
-//     assert.deepEqual([1], C.head(fill));
-//   });
-//
-// });
-//
-// describe('#C.tail',() => {
-//   it('should return [ 2, 3, 4 ] when the array is [1,2,3,4]', () => {
-//     assert.deepEqual([2, 3, 4], C.tail(fill));
-//   });
-//   it('should return [ 2, [ 3, 4 ] ] when the array is [1,2,[3,4]]', () => {
-//     assert.deepEqual([ 2, [ 3, 4 ] ], C.tail(nested));
-//   });
-//
-// });
-//
-// describe('#C.flatten',() => {
-//   it('should return [ 1, 2, 3, 4 ] when the array is [ 1, 2, 3, 4 ]', () => {
-//     assert.deepEqual([ 1, 2, 3, 4 ], C.flatten(fill));
-//   });
-//   it('should return [ 1, 2, 3, 4 ] when the array is [ 1, 2, [ 3, 4 ] ]', () => {
-//     assert.deepEqual([ 1, 2, 3, 4 ], C.flatten(nested));
-//   });
-//
-// });
-//
-//
-//
-// //
-// // const data = require('./dummy');
-// // const R = require('ramda');
-// //
-// // let findById = (id) => {
-// //   var select = x => x.id === id
-// //   return R.filter(select, data)
-// // }
-// //
-// // console.log(data);
-// // console.log(findById(1));
+describe('#findByYear',() => {
+  it('It should have the same length between movie 2001 and lib.findByYear(2001).length', () => {
+    assert.equal(data.filter(datum => datum.year === 2001).length, lib.findByYear(2001).length);
+  });
+  it('It should have the same value between movie 2001 and lib.findByYear(2001).length', () => {
+    assert.deepEqual(data.filter(datum => datum.year === 2001), lib.findByYear(2001));
+  });
+  it('It should have the same value in index 12 between movie 2001 and lib.findByYear(2001).length', () => {
+    assert.deepEqual(data.filter(datum => datum.year === 2001)[12], lib.findByYear(2001)[12]);
+  });
+});
+
+describe('#findByTitle',() => {
+  it('It should give the same data given the same title', () => {
+    assert.deepEqual(data.filter(datum => datum.title === 'Phone Booth'), lib.findByTitle('Phone Booth'));
+  });
+});
